f058b1e680ecc7d416734608c533f058f50c3600
JustThomi <t.wild155@gmail.com>
2026-07-23 12:37:48 +0300

    add basic order selection and sprites

--- src/ui/hud.gd
+++ src/ui/hud.gd
@@ -1,9 +1,20 @@
 extends Control
 
 @onready var phone: MarginContainer = $Phone
+@onready var orders_page: MarginContainer = $Phone/OrdersPage
+@onready var active_order_page: MarginContainer = $Phone/ActiveOrderPage
+
+func _ready() -> void:
+	orders_page.show()
+	active_order_page.hide()
 
 func _on_area_2d_mouse_entered():
 	phone.position.y = 392.0
 
 func _on_area_2d_mouse_exited():
 	phone.position.y = 500.0
+
+func _on_button_pressed() -> void:
+	# TODO: trigger orders here
+	orders_page.hide()
+	active_order_page.show()
