efdd734db3f6a8cae97ff0b12505311cf7e15418
Robert Varga <rvarga253@yahoo.com>
2026-07-25 11:06:02 +0300

    Added lightning flash, changed colors and lights for weather

--- src/weather/weather_manager.gd
+++ src/weather/weather_manager.gd
@@ -7,6 +7,9 @@ enum Weather { CLEAR, RAIN, STORM, FOG }
 var multipliers = { Weather.CLEAR: 1.0, Weather.RAIN: 1.25, Weather.STORM: 2.0, Weather.FOG: 1.5 }
 
 var current_weather: Weather = Weather.CLEAR
+var lightning_running := false
+
+signal lightning
 
 @export var min_change_time := 20
 @export var max_change_time := 60
@@ -27,7 +30,23 @@ func change_random_weather():
 	var possible_weathers = [Weather.CLEAR, Weather.CLEAR, Weather.CLEAR, Weather.RAIN, Weather.FOG, Weather.STORM]
 	
 	current_weather = possible_weathers.pick_random()
+	if current_weather == Weather.STORM and !lightning_running:
+		lightning_loop()
+		
 	weather_changed.emit(current_weather)
 
 func get_payment_multiplier() -> float:
 	return multipliers[current_weather]
+
+func lightning_loop() -> void:
+	lightning_running = true
+
+	while current_weather == Weather.STORM:
+		await get_tree().create_timer(randf_range(2.0, 8.0)).timeout
+
+		if current_weather != Weather.STORM:
+			break
+
+		lightning.emit()
+
+	lightning_running = false
