7217e28d1ce51c8f4623edc6f1354e33fee2cadc
Cristi <maftei.cristian23@gmail.com>
2026-07-25 21:35:14 +0300

    NPC Spawner Works on New Map

--- src/crowd/crowd_manager.gd
+++ src/crowd/crowd_manager.gd
@@ -2,23 +2,25 @@ extends Node3D
 
 @export var pedestrian_scene : PackedScene
 @export var police_scene : PackedScene
-@export var total_pedestrians : int = 30
-@export var total_police : int = 5
+@export var total_pedestrians : int = 50
+@export var total_police : int = 10
+@export var spawn_radius : float = 100.0
 
 func _ready():
 	randomize()
-	
 	var map = get_world_3d().navigation_map
-	while map == RID():
+	
+	NavigationServer3D.map_force_update(map)
+	
+	while NavigationServer3D.map_get_random_point(map, 1, false) == Vector3.ZERO:
 		await get_tree().physics_frame
-		map = get_world_3d().navigation_map
-	
-	spawn_on_navmesh()
+		NavigationServer3D.map_force_update(map)
+		
+	spawn_on_navmesh(map)
 
-func spawn_on_navmesh():
-	var map = get_world_3d().navigation_map
-	_spawn_entities(pedestrian_scene, total_pedestrians, map)
-	_spawn_entities(police_scene, total_police, map)
+func spawn_on_navmesh(map: RID):
+	await _spawn_entities(pedestrian_scene, total_pedestrians, map)
+	await _spawn_entities(police_scene, total_police, map)
 
 func _spawn_entities(scene_to_spawn: PackedScene, amount: int, map: RID):
 	if not scene_to_spawn:
@@ -27,11 +29,13 @@ func _spawn_entities(scene_to_spawn: PackedScene, amount: int, map: RID):
 	for i in range(amount):
 		var new_entity = scene_to_spawn.instantiate()
 		
-		var random_x = randf_range(-50.0, 50.0) 
-		var random_z = randf_range(-50.0, 50.0)
+		var random_x = randf_range(-spawn_radius, spawn_radius) 
+		var random_z = randf_range(-spawn_radius, spawn_radius)
 		var search_point = Vector3(random_x, 0.0, random_z)
 		
 		var safe_point = NavigationServer3D.map_get_closest_point(map, search_point)
 		
 		new_entity.position = safe_point
 		add_child(new_entity)
+		
+		await get_tree().physics_frame
